670f028ac642911c84b779fe820c2ec7773fcc21
Bijay Gyawali <bijayhyr@Bijays-Air.local>
2020-09-09 18:15:39 -0500

    changes

--- src/components/pages/contact/ContactMe.js
+++ src/components/pages/contact/ContactMe.js
@@ -6,7 +6,7 @@ export default class ContactMe extends Component {
         return (
             <div className="contactme-form">
                 <form name="contact" method="POST" data-netlify="true">
-                <input type='hidden' name='contact' value='contact' />
+                <input type='hidden' name='form-name' value='contact' />
                 <p>
                     <label>Your Name: <input type="text" name="name" /></label>   
                 </p>
